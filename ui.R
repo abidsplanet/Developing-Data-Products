24d9b25cdfc1ba4ab6058af4d1aa73d61f601e71
Abidur Rahman mallik <abidsplanet@gmail.com>
2014-11-26 03:11:12 +0600

    edited

--- ui.R
+++ ui.R
@@ -1,35 +1,23 @@
-library(shiny)
-require(rCharts)
+require(rNVD3)
+
 shinyUI(
-        navbarPage("IRIS and AIRQUALITY Database Explorer",
-                tabPanel("Iris",
-                        sidebarPanel(
-                                selectInput(inputId = "x",
-                                    label = "Choose X",
-                                    choices = c('SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'),
-                                    selected = "SepalLength"),
-                                selectInput(inputId = "y",
-                                    label = "Choose Y",
-                                    choices = c('SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'),
-                                    selected = "SepalWidth")
-                        ),
-                mainPanel(
-                        showOutput("myChart", "polycharts")
-                        )
-                ),
-                tabPanel("Air Quality",
+        navbarPage("HairEyeColor Database Explorer",
+                tabPanel("HairEyeColor",
                          sidebarPanel(
-                                 selectInput(inputId = "x",
-                                             label = "Choose X",
-                                             choices = c('SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'),
-                                             selected = "SepalLength"),
-                                 selectInput(inputId = "y",
-                                             label = "Choose Y",
-                                             choices = c('SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'),
-                                             selected = "SepalWidth")
+                           selectInput(inputId = "gender",
+                                       label = "Choose Gender",
+                                       choices = c("Male", "Female"),
+                                       selected = "Male"),
+                           selectInput(inputId = "type",
+                                       label = "Choose Chart Type",
+                                       choices = c("multiBarChart", "multiBarHorizontalChart"),
+                                       selected = "multiBarChart"),
+                           checkboxInput(inputId = "stack",
+                                         label = strong("Stack Bars?"),
+                                         value = FALSE)
                          ),
                          mainPanel(
-                                 showOutput("myChart", "polycharts")
+                           showOutput("myChart")
                          )
                 ),
                 
